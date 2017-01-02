4ad243afcfa0af7dd477796c6059d575e65ac073
Benedikt Pfaff <benedikt.pfaff@stud-mail.uni-wuerzburg.de>
2017-01-02 16:37:02 +0100

    AppWebMeasurements: try to fix Rest endpoint

--- src/main/java/org/sardineproject/sbyod/rest/AppWebMeasurement.java
+++ src/main/java/org/sardineproject/sbyod/rest/AppWebMeasurement.java
@@ -2,6 +2,7 @@ package org.sardineproject.sbyod.rest;
 
 import com.fasterxml.jackson.databind.deser.std.MapEntryDeserializer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.sun.jersey.core.impl.provider.entity.StringProvider;
 import org.onosproject.rest.AbstractWebResource;
 import org.sardineproject.sbyod.measurement.Measurement;
 import org.sardineproject.sbyod.portal.PortalManager;
@@ -18,8 +19,8 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Define Settings for Measurements
  */
 @Path("/measurements")
-@Produces(MediaType.TEXT_PLAIN)
-@Consumes(MediaType.TEXT_PLAIN)
+@Produces("text/plain")
+@Consumes("text/plain")
 public class AppWebMeasurement extends AbstractWebResource {
 
     private static final Logger log = getLogger(PortalManager.class);
@@ -48,13 +49,13 @@ public class AppWebMeasurement extends AbstractWebResource {
     }
 
     /**
-     * Define the logFile Location
+     * Returns the logFile Location
      * @return logFile location
      */
     @GET
     @Path("/")
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces("text/plain")
+    @Consumes("text/plain")
     public Response getLogFile() {
 
         Measurement measurement = get(Measurement.class);
