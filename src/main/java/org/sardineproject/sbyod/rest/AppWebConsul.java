1e4323d7b39153a44acd8bdfcd349906fefd848e
Benedikt Pfaff <benedikt.pfaff@stud-mail.uni-wuerzburg.de>
2017-05-31 15:24:27 +0200

    changes in rest consul and service

--- src/main/java/org/sardineproject/sbyod/rest/AppWebConsul.java
+++ src/main/java/org/sardineproject/sbyod/rest/AppWebConsul.java
@@ -33,7 +33,11 @@ import org.sardineproject.sbyod.service.Service;
 import org.sardineproject.sbyod.service.ServiceStore;
 import org.slf4j.Logger;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
