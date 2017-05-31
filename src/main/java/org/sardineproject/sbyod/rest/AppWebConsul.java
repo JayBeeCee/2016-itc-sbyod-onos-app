0eb940d24204bf91132a09ef6528b5c9b0f4d7ab
Benedikt Pfaff <benedikt.pfaff@stud-mail.uni-wuerzburg.de>
2017-05-31 15:45:18 +0200

    changes in rest consul and service

--- src/main/java/org/sardineproject/sbyod/rest/AppWebConsul.java
+++ src/main/java/org/sardineproject/sbyod/rest/AppWebConsul.java
@@ -33,7 +33,13 @@ import org.sardineproject.sbyod.service.Service;
 import org.sardineproject.sbyod.service.ServiceStore;
 import org.slf4j.Logger;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
@@ -139,7 +145,7 @@ public class AppWebConsul extends AbstractWebResource{
      * Disconnecting from Consul as service discovery client.
      */
     @DELETE
-    @Path("")
+    @Path("/delete")
     public Response deactivateConsul(){
         log.debug("AppWebConsul: Deactivating consul service discovery");
 
